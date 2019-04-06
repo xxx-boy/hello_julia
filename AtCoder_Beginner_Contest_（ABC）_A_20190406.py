140878ef15bb548d3b822bde58e5affcbe612fb5
xxx_boy <happycatcat23@gmail.com>
2019-04-06 23:33:27 +0900

    回答A

---
+++ AtCoder_Beginner_Contest_（ABC）_A_20190406.py
@@ -0,0 +1,12 @@
+a = parse(Int,readline())
+b = parse(Int,readline())
+c = parse(Int,readline())
+d = parse(Int,readline())
+e = parse(Int,readline())
+k = parse(Int,readline())
+
+if e-a > k        
+    print(":(")
+else
+    print("Yay!")
+end
